17aff98464216d70ff270f279c39b443a16db353
Aaron Hertner <aaron36hertner@gmail.com>
2019-05-18 21:52:04 -0400

    Initial commit

---
+++ Assets/Scipts/PlayerFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 offset = new Vector3(0, 2, -6);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/Scipts/movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    //member variables of class movement
+    public Rigidbody    rb;
+    private float       force = 1000f;
+    private string[]    keys = {"a", "d"};
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //forward movement
+        rb.AddForce(0, 0, 8000);
+
+        //left movement
+        if (Input.GetKey(keys[0]))
+        {
+            rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        //right movement
+        if (Input.GetKey(keys[1]))
+        {
+            rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
